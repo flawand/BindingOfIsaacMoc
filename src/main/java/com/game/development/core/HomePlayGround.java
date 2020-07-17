2314b9c02296dd33150ef21e6d54e4d9b84b7d01
fadilawand <fadifrancais@hotmail.com>
2020-07-16 20:17:58 -0400

    -Deleted TODO comment and renamed a testcase.

--- src/main/java/com/game/development/core/HomePlayGround.java
+++ src/main/java/com/game/development/core/HomePlayGround.java
@@ -7,7 +7,6 @@ import javax.swing.border.BevelBorder;
 public class HomePlayGround extends StandardFrame {
 
 
-    //TODO use Standard Frame to create this screen with a bed instead of entrance door
     private HomePlayGroundPanel homePlayGroundPanel;
 
     @Override
--- src/test/java/com/game/development/utilities/StandardFrameTest.java
+++ src/test/java/com/game/development/utilities/StandardFrameTest.java
@@ -1,26 +1,17 @@
 package com.game.development.utilities;
 
-import com.game.development.core.HomePlayGround;
-import com.game.development.core.HomePlayGroundPanel;
 import org.junit.jupiter.api.Test;
 
-import javax.swing.*;
 
 import static java.lang.Thread.sleep;
-import static org.junit.jupiter.api.Assertions.*;
 
-
-//TODO create an instance of the frame and ensure GUI matches requiremnts
 
 class StandardFrameTest {
 
     @Test
-    void initPanel() throws InterruptedException {
+    void testStandardFrame() throws InterruptedException {
         StandardFrame standardFrame = new StandardFrame();
         sleep(10000);
     }
 
-    @Test
-    void initFrame() {
-    }
 }
